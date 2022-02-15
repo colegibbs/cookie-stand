d180f253ac9ab2f04d081ed7a923cdc8ccb309cd
Cole Gibbs <colegibbs0@gmail.com>
2022-02-14 16:21:22 -0800

    created cookieSum property and relative method for suming and sum function

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ let seattle = {
   avgCookiesPerCust: 6.3,
   cust: 0,
   cookiesPurchased: [],
+  totalCookies: 0,
+  cookieSum: function(){
+    this.totalCookies = sum(this.cookiesPurchased);
+  },
   custGenerator: function(){
     this.cust = randomCust(this.minCustPerHour, this.maxCustPerHour);
   }
@@ -22,6 +26,10 @@ let tokyo = {
   avgCookiesPerCust: 1.2,
   cust: 0,
   cookiesPurchased: [],
+  totalCookies: 0,
+  cookieSum: function(){
+    this.totalCookies = sum(this.cookiesPurchased);
+  },
   custGenerator: function(){
     this.cust = randomCust(this.minCustPerHour, this.maxCustPerHour);
   }
@@ -34,6 +42,10 @@ let dubai = {
   avgCookiesPerCust: 3.7,
   cust: 0,
   cookiesPurchased: [],
+  totalCookies: 0,
+  cookieSum: function(){
+    this.totalCookies = sum(this.cookiesPurchased);
+  },
   custGenerator: function(){
     this.cust = randomCust(this.minCustPerHour, this.maxCustPerHour);
   }
@@ -46,6 +58,10 @@ let paris = {
   avgCookiesPerCust: 2.3,
   cust: 0,
   cookiesPurchased: [],
+  totalCookies: 0,
+  cookieSum: function(){
+    this.totalCookies = sum(this.cookiesPurchased);
+  },
   custGenerator: function(){
     this.cust = randomCust(this.minCustPerHour, this.maxCustPerHour);
   }
@@ -58,10 +74,15 @@ let lima = {
   avgCookiesPerCust: 4.6,
   cust: 0,
   cookiesPurchased: [],
+  totalCookies: 0,
+  cookieSum: function(){
+    this.totalCookies = sum(this.cookiesPurchased);
+  },
   custGenerator: function(){
     this.cust = randomCust(this.minCustPerHour, this.maxCustPerHour);
   }
 };
+
 //call custGenerator and create array for cookies sold for each city
 function cookiesSold(){
   let cities = [seattle, tokyo, dubai, paris, lima];
@@ -72,6 +93,7 @@ function cookiesSold(){
       let cookies = Math.ceil(Number(city.cust) * Number(city.avgCookiesPerCust));
       city.cookiesPurchased.push(cookies);
     }
+    city.cookieSum();
   }
 }
 
@@ -80,11 +102,25 @@ function randomCust(min,max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function sum(arr){
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
+}
+
 cookiesSold();
 
 console.log(seattle.cookiesPurchased);
+console.log(seattle.totalCookies);
 console.log(tokyo.cookiesPurchased);
+console.log(tokyo.totalCookies);
 console.log(dubai.cookiesPurchased);
+console.log(dubai.totalCookies);
 console.log(paris.cookiesPurchased);
+console.log(paris.totalCookies);
 console.log(lima.cookiesPurchased);
+console.log(lima.totalCookies);
+
 //call
