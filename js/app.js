9b655733101953f1a9965cb2ff8418e312581cdd
Cole Gibbs <colegibbs0@gmail.com>
2022-02-15 09:36:42 -0800

    checking somthing for lab 06 branch

--- js/app.js
+++ js/app.js
@@ -110,6 +110,7 @@ function randomCust(min,max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+//sum fucntion
 function sum(arr){
   let sum = 0;
   for(let i = 0; i < arr.length; i++){
